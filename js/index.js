ebca491b4f0a6f5c6b8349b31bff8b9e3ecdbf55
Megan Beyl <231206@virtualwindow.co.za>
2023-05-24 14:20:15 +0200

    Start

---
+++ js/index.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+newFunction = () => {
+    let valOne = 5;
+    let valTwo = 6;
+    let valueOne = valOne;
+    let valueTwo = valTwo;
+
+    console.log(valueOne = valueTwo);
+}
+
+newFunction(6,2);
+
